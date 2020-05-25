817562b66642f613ac1df5d893b97bdd5239a0bf
dannypas00 <dannypas00@outlook.com>
2020-05-25 13:27:30 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A_Star_pathfinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int gridWidth = 10, gridHeight = 15;
+        public static Node[,] buttonGrid = new Node[gridWidth, gridHeight];
+        public static Node start, end;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            VisualGrid.Height = mainWindow.Height / gridHeight * gridWidth;
+            VisualGrid.Width = mainWindow.Height / gridHeight * gridHeight;
+            for (int i = 0; i < gridWidth; i++)
+            {
+                VisualGrid.RowDefinitions.Add(new RowDefinition() { Name = "Row" + i });
+            }
+            for (int j = 0; j < gridHeight; j++)
+            {
+                VisualGrid.ColumnDefinitions.Add(new ColumnDefinition() { Name = "Col" + j });
+                for (int i = 0; i < gridWidth; i++)
+                {
+                    Button button = new Button();
+                    VisualGrid.Children.Add(button);
+                    Grid.SetColumn(button, j);
+                    Grid.SetRow(button, i);
+                    buttonGrid[i, j] = new Node(i, j, button);
+                    if (i == 5 && j == 8)
+                    {
+                        int targetRow = 0;
+                        int targetCol = 0;
+                        Trace.WriteLine("Distance from " + i + ", " + j + " to " + targetRow + ", " + targetCol + " is " + buttonGrid[i, j].DistanceToNode(buttonGrid[targetRow, targetCol]) + ".");
+                    }
+                }
+            }
+            start = buttonGrid[8, 12];
+            end = buttonGrid[1, 1];
+            start.MakeStartPoint();
+            end.MakeEndPoint();
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace A_Star_pathfinding
+{
+    public class Node
+    {
+        private static int distanceDiagonal = 14;
+        private static int distanceStraight = 10;
+        private readonly int row;
+        private readonly int col;
+        private readonly Button button;
+        private int GCost; //Distance from starting node
+        private int HCost; //Distance from End node
+        private int FCost; //GCost + HCost
+        private bool closed = false;
+        private bool traversible = true;
+
+        public Node(int row, int col, Button button)
+        {
+            this.row = row;
+            this.col = col;
+            this.button = button;
+            button.PreviewMouseDown += Button_MouseDown;    //Has to be preview, because the Click event eats up the left mouse click
+            button.Background = default;
+        }
+
+        public void MakeStartPoint()
+        {
+            button.Background = Brushes.Cyan;
+        }
+
+        public void MakeEndPoint()
+        {
+            button.Background = Brushes.Cyan;
+        }
+
+        private void Button_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!closed && e.ChangedButton == MouseButton.Left)
+            {
+                button.Background = Brushes.Red;
+                closed = true;
+            } else if (e.ChangedButton == MouseButton.Right)
+            {
+                if (traversible)
+                {
+                    button.Background = Brushes.Black;
+                } else
+                {
+                    button.Background = default;
+                }
+                traversible = !traversible;
+            }
+        }
+
+        public int DistanceToNode(Node node)
+        {
+            if (this == node)
+            {
+                //button.Background = Brushes.DarkBlue;
+                return 0;
+            }
+            int rowDir = node.GetRow() - row;
+            int colDir = node.GetCol() - col;
+            return MainWindow.buttonGrid[row + Math.Sign(rowDir), col + Math.Sign(colDir)].DistanceToNode(node) + ((rowDir != 0 && rowDir != 0) ? distanceDiagonal : distanceStraight);
+        }
+
+        public int GetCol()
+        {
+            return col;
+        }
+
+        public int GetRow()
+        {
+            return row;
+        }
+
+        public bool IsTraversible()
+        {
+            return traversible;
+        }
+    }
+}
